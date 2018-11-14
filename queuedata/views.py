795dacff3e9e9b0f2159de3497edc83265261cbf
GoeschWho <mlgoesch11@gmail.com>
2018-11-14 11:17:20 -0600

    Django templates

--- queuedata/views.py
+++ queuedata/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Lab
 
 def lab_list(request):
-    return render(request, 'queuedata/lab_list.html', {})
+    labs = Lab.objects.all()
+    return render(request, 'queuedata/lab_list.html', {'labs': labs})
