b409ecaec65a5bb956120ecd083e6ce1695d937b
GoeschWho <mlgoesch11@gmail.com>
2018-11-14 11:38:12 -0600

    Revering for test

--- queuedata/views.py
+++ queuedata/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Lab
 
 def lab_list(request):
-    labs = Lab.objects.all()
-    return render(request, 'queuedata/lab_list.html', {'labs': labs})
+    #labs = Lab.objects.all()
+    return render(request, 'queuedata/lab_list.html', {})
