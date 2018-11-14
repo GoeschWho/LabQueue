7db5a08cca4b7b009d1304c687b8ac7b1012b51b
GoeschWho <mlgoesch11@gmail.com>
2018-11-14 14:43:44 -0600

    Added LabParts module

--- queuedata/models.py
+++ queuedata/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+
 class Lab(models.Model):
     lab_number = models.IntegerField()
     lab_name = models.TextField()
 
     def __str__(self):
         return str(self.lab_number) + " - " + self.lab_name
+
+
+class LabParts(models.Model):
+    lab = Lab
+    part_name = models.TextField()
+
+    def __str__(self):
+        return "Lab " + str(self.lab.lab_number) + " - " + self.part_name
