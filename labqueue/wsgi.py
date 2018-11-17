08a75749a42ac4ea48bbb17ffc70816322cac6d5
GoeschWho <mlgoesch11@gmail.com>
2018-11-16 22:31:51 -0600

    Add 'queue' to sys.path

--- labqueue/wsgi.py
+++ labqueue/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labqueue.settings')
 
 application = get_wsgi_application()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.join(BASE_DIR, 'queue')
+
+if path not in sys.path:
+    sys.path.insert(0, path)
